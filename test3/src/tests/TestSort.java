c8fdc8b13d18040b88d92d8d2b3c51bfb0f9a70b
cajic95 <61760480+cajic95@users.noreply.github.com>
2021-06-04 01:44:22 +0200

    Update TestSort.java
    refactor testLoging2

--- test3/src/tests/TestSort.java
+++ test3/src/tests/TestSort.java
@@ -4,7 +4,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -21,12 +25,11 @@ import org.testng.annotations.Test;
 import objs.HomePageS;
 import objs.Inventory;
 
-
 public class TestSort {
 	public static final String LOW_PRICE_ITEM = "//*[@id=\"item_2_title_link\"]/div";
-	
-private static WebDriver driver;
-	
+
+	private static WebDriver driver;
+
 	@BeforeClass
 	public void createDriver() {
 
@@ -34,53 +37,51 @@ private static WebDriver driver;
 		driver = new ChromeDriver();
 
 	}
+
 	@Test
 	public void testLoging2() {
 		File f = new File("data.xlsx");
-		
+
 		try {
 			InputStream in = new FileInputStream(f);
-			
+
 			XSSFWorkbook wb;
 			wb = new XSSFWorkbook(in);
 			Sheet sheet = wb.getSheetAt(0);
 			Row row = sheet.getRow(1);
 			Cell cell = row.getCell(0);
 			Cell cell2 = row.getCell(1);
-			
-			
+
 			driver.manage().window().maximize();
 			driver.get(HomePageS.URL);
 			HomePageS.clickUsername(driver, cell.toString());
 			HomePageS.clickPassword(driver, cell2.toString());
 			HomePageS.clickLogin(driver);
-			
+
 			Inventory.clickSort(driver);
-			
-			
-		
-			
+
 			List<WebElement> list = driver.findElements(By.className("inventory_item_price"));
-			
-			
-			
-			String actualPrice1= list.get(0).getText();;
-			String expectedPrice1 = "$7.99";
-			String actualPrice2 = list.get(list.size()-1).getText();
-			String expectedPrice2 = "$49.99";
-			
-			Assert.assertEquals(actualPrice1, expectedPrice1);
-			Assert.assertEquals(actualPrice2, expectedPrice2);
-			
+
+			List<String> asc = new ArrayList<String>();
+			for (int i = 0; i < list.size(); i++) {
+				asc.add(list.get(i).getText().substring(1));
+			}
+			List<Double> listOfDouble = Inventory.convertStringListToDoubleList(asc, Double::parseDouble);
+			System.out.println("Lista" + listOfDouble.toString());
+
+			List<Double> ascSorted = new ArrayList<Double>(listOfDouble);
+			ascSorted.sort(Comparator.naturalOrder());
+			System.out.println("Lista" + ascSorted.toString());
+
+			Assert.assertEquals(listOfDouble, ascSorted);
+
 			wb.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		driver.close();
-		
-		
-	}
 
+	}
 
 }
